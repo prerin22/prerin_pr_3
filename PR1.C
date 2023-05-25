13ca93b9fc29b2530f522447edd09d174efbd4d0
Parth Bhadani <parthbhadani8467@gmail.com>
2023-05-25 12:51:02 +0530

    pr2

---
+++ PR1.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+int a;
+clrscr();
+printf("enter a number:");
+scanf("%d",&a);
+a%2==0? printf("even number\n"): printf("odd number\n");
+
+getch();
+
+
+}
